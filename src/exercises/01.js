e450d79fed267eccf93aa534b8df2238ee9c2833
Brendan Shulman <brendan.shulman@gmail.com>
2022-01-14 20:56:39 -0500

    refactored example 1

--- src/exercises/01.js
+++ src/exercises/01.js
@@ -1,14 +1,14 @@
-import { Component } from "react";
+import { Component } from 'react';
 
 class ClassComp01 extends Component {
-    render() {
-        const { imageSource } = this.props;
-        return <img alt="zzzzzzzzzzzz" src={imageSource} />;
-    }
+  render() {
+    const { imageSource } = this.props;
+    return <img alt="zzzzzzzzzzzz" src={imageSource} />;
+  }
 }
 
-const FunctionalComponent01 = () => {
-    return null;
+const FunctionalComponent01 = (props) => {
+  return <img alt="zzzzzzzzzzzz" src={props.imageSource} />;
 };
 
-export default ClassComp01;
+export default FunctionalComponent01;
